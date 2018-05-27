29236ad80fd2d91b982bd19df366ac345c576a9b
Fanny <fannybecerravera@gmail.com>
2018-05-26 22:16:41 -0400

    Subiendo ejercicio ordenando comida

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+switch (prompt("¿Cuál es el plato que deceas ordenar?")) {
+    case"ceviche":
+      alert("¡Lo siento!No se encuentra disponible este plato por el momento");
+      break;
+    case"tacos al pastor":
+    alert("¡Lo siento! No se encuentra disponible este plato por el momento");
+    case"empanada chilena":
+    alert("¡Este plato si esta diponible!");
+    break;
+    default:
+    alert("En caso de que no este el plato que te guste te ofrecemos hambuerguesas");
+}
+
+
+    
+    
