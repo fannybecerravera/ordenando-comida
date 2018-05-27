8d71b1591d1506072176bb0798b09cdab280a86d
Fanny <fannybecerravera@gmail.com>
2018-05-26 22:21:32 -0400

    agregando comentario

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
-switch (prompt("¿Cuál es el plato que deceas ordenar?")) {
+/*Imagina que estás pidiendo comida en un restaurante.
+ Tu plato favorito es ceviche, así que lo elegirías si está en el menú.
+ Si no hay ceviche, elegirías tacos al pastor. 
+ Si eso no está, te irías por empanada chilena. 
+ En el caso raro que ninguna de esas opciones esté disponible, te irías por una hamburguesa. 
+ Escribe una función, que de acuerdo a la disponibilidad de los platos en el menú, decidas qué ordenar. */
+
+ switch (prompt("¿Cuál es el plato que deceas ordenar?")) {
     case"ceviche":
       alert("¡Lo siento!No se encuentra disponible este plato por el momento");
       break;
